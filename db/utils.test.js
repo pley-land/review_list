e0078c716d61f5b795e1a985b395a7a123790edf
Enji <enjikim17@gmail.com>
2018-07-26 13:16:17 -0700

    Adjust utils test

--- db/utils.test.js
+++ db/utils.test.js
@@ -2,5 +2,5 @@ const utils = require('./utils.js');
 
 const { generateCool } = utils;
 test('generates a Cool count for a review', () => {
-  expect(generateCool()).toBe();
+	expect(generateCool()).to.be.within(0, 4);
 });
