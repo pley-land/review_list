606ccfd698e55e2834e7e583497155c8230bc084
Enji <enjikim17@gmail.com>
2018-07-28 23:23:37 -0700

    Connect main react component with index.html

---
+++ client/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          TEST
+        </h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
